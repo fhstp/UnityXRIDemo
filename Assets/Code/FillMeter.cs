381e2fb29058e3765610c64930bb9ba78d56c311
ComradeVanti <kingvanti650@gmail.com>
2023-11-17 10:54:14 +0100

    feat: open door

--- Assets/Code/FillMeter.cs
+++ Assets/Code/FillMeter.cs
@@ -10,6 +10,7 @@ namespace At.Ac.FhStp.XRIDemo
     public class FillMeter : MonoBehaviour
     {
         [SerializeField] private UnityEvent<float> onFillChanged;
+        [SerializeField] private UnityEvent onFull;
         [SerializeField] private float changeIncrement;
 
         private float fill;
@@ -21,6 +22,8 @@ namespace At.Ac.FhStp.XRIDemo
 
             fill = newFill;
             onFillChanged.Invoke(newFill);
+
+            if (Math.Abs(fill - 1) < Mathf.Epsilon) onFull.Invoke();
         }
 
         private void Awake()
